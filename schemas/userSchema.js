3bf97d804e50cb016a8e7557630f33b99e43c18c
Sofiia <139391583+Cynosure24@users.noreply.github.com>
2024-04-28 23:12:40 +0200

    Update userSchema.js

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -4,6 +4,7 @@ export const registerSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
+
 export const loginSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
