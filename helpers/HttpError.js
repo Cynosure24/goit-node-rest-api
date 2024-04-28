af08e541828b7c29bc9e3a9c91bb983d9511ea6a
Sofiia <139391583+Cynosure24@users.noreply.github.com>
2024-04-28 17:33:28 +0200

    update

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,15 +1,15 @@
 const messageList = {
-    400: "Bad Request",
-    401: "Unauthorized",
+    400: "Помилка від Joi або іншої бібліотеки валідації",
+    401: "Not authorized",
     403: "Forbidden",
     404: "Not Found",
-    409: "Conflict",
-}
-
-const HttpError = (status, message = messageList[status]) => {
+    409: "Email in use",
+  };
+  
+  const HttpError = (status, message = messageList[status]) => {
     const error = new Error(message);
     error.status = status;
     return error;
-}
-
-export default HttpError;
+  };
+  
+  export default HttpError;
