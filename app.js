698c247a1ec53e718ec71d315c01f42f4d9bd5fa
Sofiia <139391583+Cynosure24@users.noreply.github.com>
2024-05-03 22:52:28 +0200

    update

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-
+import mongoose from "mongoose";
 import contactsRouter from "./routes/contactsRouter.js";
+import dotenv from "dotenv";
+import authRouter from "./routes/authRouter.js";
 
 const app = express();
-
+dotenv.config();
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.DB_HOST)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Server is running. Use our API on port: 3000");
+    });
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+app.use(express.static("public"));
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
@@ -19,8 +36,4 @@ app.use((_, res) => {
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
-});
-
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
 });
---
+++ controllers/authController.js
@@ -0,0 +1,82 @@
+import { User } from "../models/userModel.js";
+import HttpError from "../helpers/HttpError.js";
+import ctrlWrapper from "../helpers/ctrlWrapper.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { token } from "morgan";
+import gravatar from "gravatar";
+import path from "path";
+import fs from "fs/promises";
+import Jimp from "jimp";
+dotenv.config();
+const { SECRET_KEY } = process.env;
+const avatarDir = path.join(process.cwd(), "public", "avatars");
+
+export const register = ctrlWrapper(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "OOops,email already in use");
+  }
+  const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
+  res.status(201).json({
+    email: newUser.email,
+    password: newUser.password,
+  });
+});
+
+export const login = ctrlWrapper(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password invalid");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password invalid");
+  }
+
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token });
+  res.json({
+    token,
+  });
+});
+
+export const getCurrent = ctrlWrapper(async (req, res) => {
+  const { email, subscription } = req.user;
+  res.json({ email, subscription });
+});
+
+export const logout = ctrlWrapper(async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+
+  res.json({
+    message: "Logout success",
+  });
+});
+
+export const updateAvatar = ctrlWrapper(async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const avatarImage = await Jimp.read(tempUpload);
+  await avatarImage.resize(250, 250);
+  const filname = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarDir, filname);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", filname);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+  res.json({ avatarURL });
+});
